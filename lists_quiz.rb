f5d94496850e445e5a15b20f91938cdfa38faec8
Quynh-Chi Nguyen <qcnguyen345@gmail.com>
2019-10-09 09:40:19 -0600

    did three_even? commit

--- lists_quiz.rb
+++ lists_quiz.rb
@@ -16,11 +16,19 @@ end
 # puts bigger_two([1,2],[3,4])
 # puts bigger_two([1,7],[4,4])
 def three_even?(list)
-    list.each do |n|
-        if list
+    (list.size-2).times do |n| #changed list.each to list.size-2 
+        if list[n]%2==0 && list[n+1]%2==0 && list[n+1]%2==0 #this checks if the three number next to each other are even
+            return true
+        end 
     end 
-    puts three_even?([2,1,3,5])#false
-    puts three_even?([2,4,12,5])#true
-    puts three_even?([2,1,4,6])#false
-    puts three_even?([1,4,6,4])#true
-    puts three_even?([])#false
+    return false #if the program doesn't return true by the time it reaches this line of code, a false is returned
+end 
+    # puts three_even?([2,1,3,5])#false
+    # puts three_even?([2,4,12,5])#true
+    # puts three_even?([2,1,4,6])#false
+    # puts three_even?([1,4,6,4])#true
+    # puts three_even?([])#false
+
+def series_up
+    
+end 
